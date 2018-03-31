3cffc72eeff9d208d1572620ed9f8ce1de62884c
liferlisiqi <lisiqi9310@163.com>
2018-03-31 10:27:36 +0800

    week7:3,4

--- machine-learning-ex6/ex6/ex6.m
+++ machine-learning-ex6/ex6/ex6.m
@@ -115,17 +115,17 @@ clear ; close all; clc
 %  plot the data. 
 %
 
-fprintf('Loading and Visualizing Data 3 ...\n')
+%fprintf('Loading and Visualizing Data 3 ...\n')
 
 % Load from ex6data3: 
 % You will have X, y in your environment
-load('ex6data3.mat');
+%load('ex6data3.mat');
 
 % Plot training data
-plotData(X, y);
+%plotData(X, y);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
 
@@ -135,15 +135,15 @@ pause;
 
 % Load from ex6data3: 
 % You will have X, y in your environment
-load('ex6data3.mat');
+%load('ex6data3.mat');
 
 % Try different SVM Parameters here
-[C, sigma] = dataset3Params(X, y, Xval, yval);
+%[C, sigma] = dataset3Params(X, y, Xval, yval);
 
 % Train the SVM
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-visualizeBoundary(X, y, model);
+%model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%visualizeBoundary(X, y, model);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
